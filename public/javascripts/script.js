9315dc7ebc13fee4d69bcf92c20fa70947edf3d4
jesse houghton <OiseauNoir>
2016-08-25 18:56:08 -0500

    safety commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,26 +3,20 @@
 $(document).ready(function() {
 
 
-  var joke = $('.current_joke').val()
-  console.log(joke);
+  // var joke = $('.current_joke').val()
+  // console.log(joke);
 
 
   $('#submit').on('submit', function(d) {
     d.preventDefault();
-
     $.ajax({
       url: 'http://api.icndb.com/jokes/random'
     }).done(function(results) {
-      //console.log(results);
       $('.current_joke').html(results.value.joke);
     })
+      twitter.post('statuses/update', { status: 'code || die' }, function(err, data, response) {
+      console.log(data)
+    })
   })
-
-
-
-
-  // twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  // console.log(data)
-  // })
 
 });
