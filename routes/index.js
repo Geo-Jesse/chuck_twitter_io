bb92251950c9b91d106c8b2683a4225f93f3c8b1
Chris Robbins <Jaguar85>
2016-08-25 23:33:04 -0500

    post route to twitter

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Twit = require('twit');
 
+
+
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ twitterBot.js
@@ -0,0 +1,1 @@
+[gist https://gist.github.com/videlais/28c450b9472cd3b27a75]
