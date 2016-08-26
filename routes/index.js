b1a8294a00bce249d70e4f67ada61ccea988717c
jesse houghton <OiseauNoir>
2016-08-26 09:45:37 -0500

    made new button

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,28 @@ var twitter = new Twit({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tweet a Random Chuck Norris Joke!' });
 });
 
+twitter.post('statuses/update', { status: 'code || die' }, function(err, data, response) {
+console.log(data)
+})
 module.exports = router;
+
+
+//POST Tweet
+// router.post('/', function (req, res, next) {
+//   const chuckTwit = new Tweet(req.body)
+//   chuckTwit.userId = req.user.sub;
+//   chuckTwit.save(function (err) {
+//     if (err) {
+//       res.status(500).send(err)
+//     } else {
+//       res.json(chuckTwit)
+//     }
+//   })
+// });
